76dfa5034d53a27e05d99b2630af2071a8ff4ba2
Z3nd <jonhrip9@gmail.com>
2023-08-07 16:07:17 +0300

    Moved the authorization receipt, added the environment

--- bot/main.py
+++ bot/main.py
@@ -1,23 +1,25 @@
-from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 import logging
-
-def get_creds():
-    # To obtain a service account JSON file
-    creds = Credentials.from_service_account_file("credentials.json")
-    scoped = creds.with_scopes([
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive",
-    ])
-    return scoped
+import google_sheet
+import asyncio
+import os
 
 
 def main():
     load_dotenv()
 
     # Setup logging
-    file_log = logging.FileHandler('log/chat.log')
+    file_log = logging.FileHandler('chat.log')
     console_out = logging.StreamHandler()
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
                         handlers=(file_log, console_out))
+
+    sheet_config = {'table_id': os.environ['SPREADSHEET_ID'],
+                    'task_sheet_name': os.environ['TASK_SHEET_NAME'],
+                    'task_begin_column': os.environ.get('TASK_BEGIN_COLUMN', 'A1')}
+
+    gs = google_sheet.SheetGoogle(sheet_config)
+
+
+if __name__ == '__main__':
+    main()
