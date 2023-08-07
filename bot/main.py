fa2836019d8c8abce95dafd197173910af447914
Z3nd <jonhrip9@gmail.com>
2023-08-07 12:12:25 +0300

    added logging

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
+import logging
 
 def get_creds():
     # To obtain a service account JSON file
@@ -12,8 +13,11 @@ def get_creds():
     return scoped
 
 
-
 def main():
     load_dotenv()
 
-
+    # Setup logging
+    file_log = logging.FileHandler('log/chat.log')
+    console_out = logging.StreamHandler()
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+                        handlers=(file_log, console_out))
