2071ab26c68375d160e15e116a97bc80ec04f818
Z3nd <jonhrip9@gmail.com>
2023-08-13 14:29:06 +0300

    skill list update method

--- bot/admin.py
+++ bot/admin.py
@@ -22,3 +22,22 @@ class Admin:
                 return result
             else:
                 logging.warning('Admin %s was not found', id_telegram)
+
+    async def skills_update(self, list_user):
+        """
+        Updating user skills
+        :param list_user: User list
+        :return:
+        """
+        try:
+            async with aiosqlite.connect(self.db) as cursor:
+                for i in list_user:
+                    await cursor.execute(f"""
+                                    UPDATE user
+                                    SET (skill) = ('{i[2]}')
+                                    where id == {i[1]} 
+                    """)
+                await cursor.commit()
+        except Exception as e:
+            logging.error('An error occurred during output_skill_counter method execution: %s', e)
+            raise e
