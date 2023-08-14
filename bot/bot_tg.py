f0aed3fc0d5274d4d7442ca345666bbdda236102
Z3nd <jonhrip9@gmail.com>
2023-08-14 16:47:32 +0300

    Create new class BotTelegram and method start

---
+++ bot/bot_tg.py
@@ -0,0 +1,78 @@
+import asyncio
+
+from aiogram import Bot
+from aiogram import types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils import executor
+from admin import Admin
+from user import User
+from google_sheet import SheetGoogle
+
+
+class BotTelegram:
+    def __init__(self, config: dict, gs, db_admin, db_user):
+        """
+        Bot initialization with the given configuration
+        :param config: dictionary with bot configurations
+        :param gs: SheetGoogle object
+        :param db_admin: Admin object
+        :param db_user: User object
+        """
+        self.storage = MemoryStorage()
+        self.bot = Bot(token=config["token_bot"])
+        self.superusers = config["superusers"].split(",")
+        self.dp = Dispatcher(self.bot, storage=self.storage)
+        self.gs: SheetGoogle = gs
+        self.db_admin: Admin = db_admin
+        self.db_user: User = db_user
+        self.button_comment = [
+            KeyboardButton('нет тикетов'),
+            KeyboardButton('нет смен'),
+            KeyboardButton('уволен'),
+            KeyboardButton('больничный'),
+            KeyboardButton('отпуск/обс'),
+        ]
+        self.get_task_butt = KeyboardButton('Получить задание')
+        self.admin_button = [
+            KeyboardButton('Выгрузка'),
+            KeyboardButton('Список аудиторов'),
+            KeyboardButton('Обновить аудиторов'),
+            KeyboardButton('Обновить навыки'),
+            KeyboardButton('Обновить админов'),
+            KeyboardButton('Ошибки'),
+            KeyboardButton('Обновить группы'),
+            KeyboardButton('Список групп'),
+            KeyboardButton('Изменить строку'),
+        ]
+        self.buttons_comment = ReplyKeyboardMarkup(resize_keyboard=True)
+        self.buttons_comment.add(*self.button_comment)
+        self.get_task = ReplyKeyboardMarkup(resize_keyboard=True)
+        self.get_task.add(self.get_task_butt)
+        self.admin_buttons = ReplyKeyboardMarkup(resize_keyboard=True)
+        self.admin_buttons.add(*self.admin_button)
+
+    async def start(self, message: types.Message):
+        """
+        Method of processing the start command
+        """
+        if str(message.from_user.id) in self.superusers or await self.db_admin.check_access(str(message.from_user.id)):
+            await message.reply(f"Привет, {message.from_user.first_name}", reply_markup=self.admin_buttons)
+        elif await self.db_user.get_name(str(message.from_user.id)):
+            await message.reply(f"Привет, {message.from_user.first_name}", reply_markup=self.get_task)
+
+    def _reg_handlers(self, dp: Dispatcher):
+        """
+        registration of message handlers
+        """
+        dp.register_message_handler(self.start, commands="start")
+
+    def run(self):
+        """
+        bot startup
+        """
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        self._reg_handlers(self.dp)
+        executor.start_polling(self.dp, skip_updates=True, loop=loop)
