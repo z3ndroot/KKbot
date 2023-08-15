6571f206d4bf76ff267e7b398f829ef3c2e7c52a
Z3nd <jonhrip9@gmail.com>
2023-08-15 17:18:08 +0300

    added new method addition_task_entry

--- bot/google_sheet.py
+++ bot/google_sheet.py
@@ -31,6 +31,8 @@ class SheetGoogle:
         self.task_begin_column = config['task_begin_column']
         self.user_sheet_name = config['user_sheet_name']
         self.admin_sheet_name = config['admin_sheet_name']
+        self.addition_sheet_name = config['addition_sheet_name']
+        self.addition_begin_column = config['addition_begin_column']
         self.__today_date = datetime.now()
 
     async def __authorize(self, table_id):
@@ -73,6 +75,25 @@ class SheetGoogle:
             logging.error('An error occurred during spreadsheet_entry method execution: %s', e)
             raise e
 
+    async def addition_task_entry(self, login, task, date, quantity):
+        """
+        Method for recording additional jobs in google tables
+        :param login: quality control login
+        :param task: accomplished task
+        :param date: task completion date
+        :param quantity:
+        :return:
+        """
+        try:
+            ss = await self.__authorize(self.table_id)
+
+            selected_sheet = await ss.worksheet(self.addition_sheet_name)
+            await selected_sheet.append_row(values=[login, task, date, quantity], table_range=self.addition_begin_column)
+        except Exception as e:
+            logging.error('An error occurred during addition_task_entry method execution: %s', e)
+            raise e
+
+
     async def google_sheet_unloading_support_rows(self):
         """
         Extracts all rows from Google table with support staff by skill and converts to a json file
@@ -83,7 +104,8 @@ class SheetGoogle:
             selected_sheet = await ss.get_worksheet(0)
             s = await selected_sheet.get_all_values()
 
-            async with aiofiles.open('google_table/work.json', "r", encoding="UTF8") as file:  # открываем json файл с навыками всех КК
+            async with aiofiles.open('google_table/work.json', "r",
+                                     encoding="UTF8") as file:  # открываем json файл с навыками всех КК
                 json_dump = await file.read()
                 json_read = json.loads(json_dump)
 
@@ -130,4 +152,3 @@ class SheetGoogle:
         except Exception as e:
             logging.error('An error occurred during administrator_list_update method execution: %s', e)
             raise e
-
