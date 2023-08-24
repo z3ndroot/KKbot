30eee615dba4ee2c08121866dcd404f9d581b8f9
Z3nd <jonhrip9@gmail.com>
2023-08-24 17:59:40 +0300

    update class UserCreate

--- bot/validators/user_validation.py
+++ bot/validators/user_validation.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class UserCreate(BaseModel):
@@ -8,3 +8,19 @@ class UserCreate(BaseModel):
     login: str
     id: int
     skill: str
+
+    @field_validator('skill')
+    @classmethod
+    def skill_not_empty(cls, v: str):
+        if not v:
+            raise ValueError("A skill can't be empty")
+        return v
+
+    @classmethod
+    def from_list(cls, data):
+        if len(data) != 3:
+            raise IndexError('List should contain 3 values')
+        login = data[0]
+        _id = data[1]
+        skill = data[2]
+        return cls(login=login, id=_id, skill=skill)
