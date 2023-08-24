be457b952a53e820bb24e250b148ebef8dc44448
Z3nd <jonhrip9@gmail.com>
2023-08-24 17:10:04 +0300

    update __init__.py

--- bot/validators/__init__.py
+++ bot/validators/__init__.py
@@ -1,3 +1,11 @@
+from .admin_validation import AdminCreate
 from .support_validation import SupportCreate
+from .task_validation import TaskCreate
+from .user_validation import UserCreate
 
-__all__ = ['SupportCreate']
+__all__ = [
+    'SupportCreate',
+    'AdminCreate',
+    'UserCreate',
+    'TaskCreate'
+]
