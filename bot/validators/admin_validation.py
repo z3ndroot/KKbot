a3176fb7f81b5bfc61863832eed66600d5e20e6b
Z3nd <jonhrip9@gmail.com>
2023-08-24 19:44:40 +0300

    update validator

--- bot/validators/admin_validation.py
+++ bot/validators/admin_validation.py
@@ -7,3 +7,12 @@ class AdminCreate(BaseModel):
     """
     login: str
     id: int
+
+    @classmethod
+    def from_list(cls, data: list):
+        if len(data) != 2:
+            raise IndexError('List should contain 2 values')
+
+        login = data[0]
+        _id = data[1]
+        return cls(login=login, id=_id)
